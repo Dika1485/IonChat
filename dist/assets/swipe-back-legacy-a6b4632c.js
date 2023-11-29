73907a8828dffcc9ee9ccb3ab1f7a5e1a400785a
Alvin Aryanta S. <a.aryanta.s@gmail.com>
2023-11-29 21:44:36 +0700

    Update android build

--- dist/assets/swipe-back-legacy-a6b4632c.js
+++ dist/assets/swipe-back-legacy-a6b4632c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c0008903.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-8283a550.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
