279bdfe8426bfa50537595c51f01925bfced8423
Dika1485 <ptiandika@gmail.com>
2023-11-29 23:29:33 +0700

    update android build

--- dist/assets/md.transition-legacy-7123f8b5.js
+++ dist/assets/md.transition-legacy-7123f8b5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8283a550.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-5b038ceb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
